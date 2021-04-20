6a0c74453a6cc55705efeaa6c08047f5aa564321
Roman Senin <roman@launchbadge.com>
2021-04-20 11:48:14 -0700

    add shopping cart screen and display total

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 
@@ -29,7 +30,8 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           debugShowCheckedModeBanner: false,
           routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => CartScreen(),
           }),
     );
   }
