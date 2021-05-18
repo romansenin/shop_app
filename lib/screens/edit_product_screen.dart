34bbef389ed2566cf32b4470d0cceef2972e7ccd
Roman Senin <roman@launchbadge.com>
2021-05-18 12:02:20 -0700

    fix: pop edit product page after update product and add product

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -114,8 +114,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
           ),
         );
       }
-      Navigator.of(context).pop();
     }
+    Navigator.of(context).pop();
   }
 
   @override
