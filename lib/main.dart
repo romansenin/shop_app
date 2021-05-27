603f60504619502adc7eadd4e85cdb46dccae3b6
Roman Senin <roman@launchbadge.com>
2021-05-26 17:33:28 -0700

    automatically log users in with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
+import 'package:shop_app/screens/splash_screen.dart';
 
 import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
@@ -51,7 +52,16 @@ class MyApp extends StatelessWidget {
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato',
             ),
-            home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+            home: auth.isAuth
+                ? ProductsOverviewScreen()
+                : FutureBuilder(
+                    future: auth.tryAutoLogin(),
+                    builder: (ctx, authResultSnapshot) =>
+                        authResultSnapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? SplashScreen()
+                            : AuthScreen(),
+                  ),
             debugShowCheckedModeBanner: false,
             routes: {
               ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
